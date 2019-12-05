89e57d1442d46dfb100139c81e94ade021aa2884
LoicMonard <monard@intechinfo.fr>
2019-12-06 00:13:15 +0100

    feat(toolbar): sets up toolbar container
    Signed-off-by: LoicMonard <monard@intechinfo.fr>

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container id="app">
+    <el-header>
+      <el-row>
+        <el-col :span="12" style="text-align: left">
+          <img src="./assets/sacred-cow.svg">
+          <span id="brand">REST</span>
+        </el-col>
+        <el-col class="right" :span="12" style="justify-content: flex-end">
+          Ok
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+  </el-container>
 </template>
 
 <style lang="scss">
+html, body {
+  margin: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Google Sans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.el-header {
+  #brand {
+    font-weight: 800;
+    font-size: 24px;
+    color: #383838;
+  }
+  padding: 10px 15% !important;
+  align-items: center;
+  div {
+    height: 100%;
+  }
+  img {
+    height: 100%;
+    margin-right: 10px;
+  }
+  .el-col {
+    display: flex;
+    align-items: center;
+  }
 }
 
 #nav {
