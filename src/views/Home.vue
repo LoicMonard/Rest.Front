44f7254cc3a0c2211e8b4b3f3e9e382755a71664
LoicMonard <monard@intechinfo.fr>
2019-12-06 00:59:24 +0100

    feat(login): creates login/signin card
    Signed-off-by: LoicMonard <monard@intechinfo.fr>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,17 +9,48 @@
       </el-col>
       <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
     </el-row>
+    
+    <el-row>
+      <el-col :span="9"/>
+      <el-col :span="6">
+        <div class="login-card">
+          <div class="header">
+            <span>Login</span>
+            <Google 
+              :size="24"
+              fillColor="#fff"/>
+          </div>
+          <div class="body">
+            <el-input placeholder="First name" v-model="firstName"></el-input>
+            <el-input placeholder="Email" v-model="lastName"></el-input>
+            <el-input placeholder="Password" v-model="password" show-password></el-input>
+          </div>
+          <div class="footer">
+            <el-button round>Connexion</el-button>
+            <el-button round>Inscription</el-button>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="9"/>
+    </el-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Google from 'vue-material-design-icons/Google.vue';
 
 export default {
   name: 'home',
+  data: () => ({
+    firstName: '',
+    lastName: '',
+    password: ''
+  }),
   components: {
-    HelloWorld
+    HelloWorld,
+    Google
   }
 }
 </script>
@@ -30,6 +61,38 @@ export default {
   .main-message {
     font-weight: 500;
     font-size: 48px;
+  }
+  .login-card {
+    margin-top: 36px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    border-radius: 4px;
+    border: 1px solid #DCDFE6;
+    .header {
+      background: #c6e2ff;
+      color: #fff;
+      font-weight: 600;
+      box-sizing: border-box;
+      padding: 24px;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-around;
+      span {
+        margin: 4px 0;
+      }
+    }
+    .body {
+      padding: 24px;
+      > * {
+        margin: 10px 0;
+      }
+    }
+    .footer {
+      margin-bottom: 24px;
+    }
   }
 }
 .el-row {
